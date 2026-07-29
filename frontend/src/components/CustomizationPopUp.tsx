1cc82bc2ab4f9f6c02f36b45fd715619f110c472
Daniil Simakov <simakov0310@gmail.com>
2026-07-29 11:51:00 +0300

    feat: finish customization ui and loading logic

--- frontend/src/components/CustomizationPopUp.tsx
+++ frontend/src/components/CustomizationPopUp.tsx
@@ -1,6 +1,6 @@
 import SyrupAndAddonChooser from './SyrupAndAddonChooser'
 import MilkChooser from './MilkChooser'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 // Для пропсов
 type PopUpProductInfo = {
@@ -96,11 +96,20 @@ export default function CustomizationPopUp({ isOpen, onClose, product, variants,
         }
     }, [options])
 
+    const scrollRef = useRef<HTMLDivElement>(null)
+
+    // Для скролла к началу карточки при открытии новой
+    useEffect(() => {
+        if (isOpen && scrollRef.current) {
+            scrollRef.current.scrollTop = 0
+        }
+    }, [isOpen])
+
 
     return (
         <div 
             className={`absolute inset-0 z-50 flex flex-col items-center justify-center 
-            bg-black/50 backdrop-blur-[4px] p-6 
+            bg-black/50 backdrop-blur-[4px] p-6
             transition-opacity duration-300 ease-[cubic-bezier(0.16,1,0.3,1)]
             ${isOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`}
         >
@@ -117,77 +126,100 @@ export default function CustomizationPopUp({ isOpen, onClose, product, variants,
             </button>
 
 
-            <div 
-                className={`relative flex flex-col w-[700px] max-h-[950px] bg-[#E4E2E2] 
-                rounded-[80px] overflow-y-auto scrollbar-hide px-10 pt-12 pb-32 
-                transition-all duration-400 ease-[cubic-bezier(0.16,1,0.3,1)]
-                ${isOpen ? "opacity-100 translate-y-0 scale-100" : "opacity-0 translate-y-12 scale-95"}`}
-            >
+                <div 
+                    className={`relative w-[700px] max-h-[950px] bg-[#E4E2E2] 
+                    rounded-[80px] overflow-hidden isolate
+                    transition-all duration-400 ease-[cubic-bezier(0.16,1,0.3,1)]
+                    ${isOpen ? "opacity-100 translate-y-0 scale-100" : "opacity-0 translate-y-12 scale-95"}`}
+                >
+                    <div ref={scrollRef}
+                    className="h-full overflow-y-auto scrollbar-hide overscroll-contain px-10 pt-12">
 
+                        <div className="relative flex -mt-15 justify-center shrink-0">
+                            <div className="absolute bottom-7 w-40 h-5 bg-black/35 rounded-full blur-md" />
+                            <img src={product?.image_url} className="relative z-10 w-96" />
+                        </div>
 
-                <div className="relative flex -mt-15 justify-center shrink-0">
-                    <div className="absolute bottom-7 w-40 h-5 bg-black/35 rounded-full blur-md" />
-                    <img src={product?.image_url} className="relative z-10 w-96" />
-                </div>
+                        <p className="text-[64px] font-semibold text-center leading-15">{product?.name}</p>
+                        <p className="text-[24px] font-medium text-[#727171] text-center mt-3">{selectedVolume} {product?.unit} • {baseKcal} ккал</p>
 
-                <p className="text-[64px] font-semibold text-center leading-15">{product?.name}</p>
-                <p className="text-[24px] font-medium text-[#727171] text-center mt-3">{selectedVolume} {product?.unit} • {baseKcal} ккал</p>
+                        <div className="w-64 h-[2px] bg-gray-500 mx-auto my-6 shrink-0" />
 
-                <div className="w-64 h-[2px] bg-gray-500 mx-auto my-6 shrink-0" />
+                        <p className="text-[20px] font-medium max-w-[450px] mx-auto text-gray-500 text-center leading-6">
+                            {product?.description}
+                        </p>
 
-                <p className="text-[20px] font-medium max-w-[400px] mx-auto text-gray-500 text-center leading-6">
-                    {product?.description}
-                </p>
+                        <p className="text-3xl font-semibold mt-8">Размер</p>
+                        <div className={`flex flex-row gap-3 mt-4`}>
+                            {variants.map((v) => {
+                                const isSelected = v.volume === selectedVolume
+                                return (
+                                    <button
+                                        key={v.id}
+                                        onClick={() => {
+                                            setSelectedVolume(v.volume)
+                                            setBaseKcal(v.calories_base)
+                                            setBasePrice(v.price_base)
+                                        }}
+                                        className={`flex-1 font-medium text-[22px] py-4 text-center rounded-full
+                                            transition-all active:scale-90 duration-300
+                                            ${isSelected 
+                                                ? "bg-black text-white cursor-default" 
+                                                : "bg-[#CBCBCB] text-[#4E4E4E] cursor-pointer"}`}
+                                    >
+                                        {v.volume} {product?.unit}
+                                    </button>
+                                )
+                            })}
+                        </div>
+                        
+                        {milk.length > 0 && 
+                            <>
+                                <p className="text-3xl font-semibold mt-8 mb-4">Тип молока</p>
+                                <MilkChooser milkInfo={milk}/>
+                            </>
+                        }
 
-                <p className="text-3xl font-semibold mt-8">Размер</p>
-                <div className={`flex flex-row gap-3 mt-4`}>
-                    {variants.map((v) => {
-                        const isSelected = v.volume === selectedVolume
-                        return (
-                            <button
-                                key={v.id}
-                                onClick={() => {
-                                    setSelectedVolume(v.volume)
-                                    setBaseKcal(v.calories_base)
-                                    setBasePrice(v.price_base)
+                        
+                            {syrups.length > 0 && 
+                                <>
+                                    <p className="text-3xl font-semibold mt-8 mb-4">Сиропы</p>
+                                    <div className="grid grid-cols-2 gap-4">
+                                        <SyrupAndAddonChooser syrupsAndAddons={syrups} />
+                                    </div>
+                                </>
+                            }
+
+                        {addons.length > 0 && 
+                            <>
+                                <p className="text-3xl font-semibold mt-8 mb-4">Добавки</p>
+                                <div className="grid grid-cols-2 gap-4">
+                                    <SyrupAndAddonChooser syrupsAndAddons={addons}/>
+                                </div>
+                            </>
+                        }
+                        
+
+                        <div className="sticky bottom-0 mt-10 z-20 bg-[#E4E2E2] pb-7">
+                            <div 
+                                className="pointer-events-none absolute -top-13 left-0 right-0 h-13"
+                                style={{
+                                    background: 'linear-gradient(to bottom, transparent, #E4E2E2)',
                                 }}
-                                className={`flex-1 font-medium text-[22px] py-4 text-center rounded-full
-                                     transition-all active:scale-90 duration-300
-                                     ${isSelected 
-                                        ? "bg-black text-white cursor-default" 
-                                        : "bg-[#CBCBCB] text-[#4E4E4E] cursor-pointer"}`}
-                            >
-                                {v.volume} {product?.unit}
-                            </button>
-                        )
-                    })}
-                </div>
-                
-                {milk.length > 0 && 
-                    <>
-                        <p className="text-3xl font-semibold mt-8 mb-4">Тип молока</p>
-                        <MilkChooser milkInfo={milk}/>
-                    </>
-                }
+                            />
 
-                <p className="text-3xl font-semibold mt-8 mb-4">Сиропы</p>
-                <SyrupAndAddonChooser/>
+                            <div className="relative flex flex-row gap-3">
+                                <div className="bg-[#CBCBCB] text-[#727171] rounded-full flex items-center justify-center text-[22px] font-medium px-8 py-4">
+                                    {selectedVolume} {product?.unit} • 230 ккал • 356₽
+                                </div>
+                                <button className="flex-1 bg-black rounded-full flex items-center justify-center text-white text-4xl">
+                                    +
+                                </button>
+                            </div>
+                        </div>
 
-                <p className="text-3xl font-semibold mt-8 mb-4">Добавки</p>
-                <SyrupAndAddonChooser/>
-
-                <div className="sticky -bottom-20 flex flex-row gap-3 px-2 z-20">
-                    <div className={`bg-[#CBCBCB] text-[#727171] rounded-full
-                        flex items-center justify-center text-[22px] font-medium
-                        px-8 py-4`}>
-                        {selectedVolume} {product?.unit} • 230 ккал • 356₽
                     </div>
-                    <button className="flex-1 bg-black rounded-full flex items-center justify-center text-white text-4xl">
-                        +
-                    </button>
                 </div>
-
-            </div>
         </div>
     )
 }
--- frontend/src/components/SyrupAndAddonChooser.tsx
+++ frontend/src/components/SyrupAndAddonChooser.tsx
@@ -1,20 +1,41 @@
-export default function SyrupAndAddonChooser() {
+type SyrupsAndAddons = {
+    id:             number
+    name:           string
+    volume:         number
+    unit:           string
+    price_delta:    number
+    calories_delta: number
+    image_url:      string
+}
+
+interface SyrupAndAddonChooserProps {
+    syrupsAndAddons: SyrupsAndAddons[]
+}
+
+export default function SyrupAndAddonChooser({ syrupsAndAddons }: SyrupAndAddonChooserProps) {
     return (
-        <div className={`flex flex-row gap-3`}>
-            <div className={`relative border-2 border-[#A2A2A2] w-25 h-25 rounded-2xl`}>
-                <div className="absolute bottom-[6px] ml-[20px] w-15 h-2 bg-black/50 rounded-full blur-[4px]" />
-                <img alt="Фото опции кастомизации" src="/options/syrups/lavender_syrup.png"
-                className={`relative z-10`} />
-            </div>
-            <div className={`flex flex-col`}>
-                <p className={`text-[24px]`}>Лавандовый</p>
-                <p className={`text-[16px] text-[#727171] -mt-1`}>20 мл • 50 ккал • 39₽</p>
-                <div className={`flex flex-row gap-3 mt-[6px]`}>
-                    <button className={`flex justify-center text-[20px] w-8 h-8 rounded-full bg-black text-white`}>-</button>
-                    <p className={`justify-center text-[20px] pt-px pl-[2px] min-w-4`}>0</p>
-                    <button className={`flex justify-center pt-px text-[20px] w-8 h-8 rounded-full bg-black text-white`}>+</button>
+        <>
+            {syrupsAndAddons.map((p) => {
+                return (
+                    <div className={`flex flex-row gap-3`}
+                     key={p.id}>
+                    <div className={`relative border-2 border-[#A2A2A2] w-25 h-25 rounded-2xl`}>
+                        <div className="absolute bottom-[6px] ml-[20px] w-15 h-2 bg-black/50 rounded-full blur-[4px]" />
+                        <img alt="Фото опции кастомизации" src={p.image_url}
+                        className={`relative z-10`} />
+                    </div>
+                    <div className={`flex flex-col`}>
+                        <p className={`text-[24px]`}>{p.name}</p>
+                        <p className={`text-[16px] text-[#727171] -mt-1`}>{p.volume} {p.unit} • {p.calories_delta} ккал • {p.price_delta}₽</p>
+                        <div className={`flex flex-row gap-3 mt-[6px]`}>
+                            <button className={`flex justify-center text-[20px] w-8 h-8 rounded-full bg-black text-white`}>-</button>
+                            <p className={`justify-center text-[20px] pt-px pl-[2px] min-w-4`}>0</p>
+                            <button className={`flex justify-center pt-px text-[20px] w-8 h-8 rounded-full bg-black text-white`}>+</button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+                )
+            })}
+        </>
     )
 }
