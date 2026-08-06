2ddd708e941d7794bc0b615ff8ab01fd1adf29f6
Daniil Simakov <simakov0310@gmail.com>
2026-08-07 02:31:03 +0300

    feat: expand logic to get all new orders instead of only one for barista

--- backend/routers/employees.go
+++ backend/routers/employees.go
@@ -12,138 +12,184 @@ import (
 )
 
 type CommonOrderInfo struct {
-	ID 		 		  int
-	OrderUUID 		  string
-	OrderClientNumber string
-	UpdatedAt 		  time.Time
+    OrderUUID         string    `json:"order_uuid"`
+    OrderClientNumber string    `json:"order_client_number"`
+    UpdatedAt         time.Time `json:"updated_at"`
+}
+
+type Product struct {
+    Name   string `json:"name"`
+    Volume string `json:"volume"`
+    Unit   string `json:"unit"`
+}
+
+type Option struct {
+    Name   string `json:"name"`
+    Group  string `json:"group"`
+    Volume string `json:"volume"`
+    Unit   string `json:"unit"`
+}
+
+type OrderPiece struct {
+    Product Product  `json:"product"`
+    Options []Option `json:"options"`
+}
+
+type Order struct {
+    CommonOrderInfo CommonOrderInfo `json:"common_order_info"`
+    Goods           []OrderPiece    `json:"goods"`
 }
 
 func RegisterEmployeesRoutes(r *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
 	r.GET("/api/barista/get-active-orders", func(c *gin.Context) {
-		// Получение общей информации о заказе
-		var commonOrderInfo CommonOrderInfo
-		err := db.QueryRow(
+		// Список айди всех новых заказов
+		var newOrdersIDs []int
+		ordersRows, err := db.Query(
 			context.Background(),
-			`SELECT id, order_uuid, order_client_number, updated_at
-			FROM orders
-			WHERE status = 'paid'`,
-		).Scan(&commonOrderInfo.ID, &commonOrderInfo.OrderUUID, 
-				&commonOrderInfo.OrderClientNumber, &commonOrderInfo.UpdatedAt)
-
-		if err != nil {
-			log.Printf("Ошибка получения основной информации о товаре из БД: %v", err)
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"error": "Ошибка базы данных",
-			})
-			return
-		}
-
-		type Product struct {
-			Name      string
-			Volume    string
-			Unit      string
-		}
-
-		type Option struct {
-			Name   string
-			Group  string
-			Volume string
-			Unit   string
-		}
-
-		type OrderPiece struct {
-			Product Product
-			Options []Option
-		}
-
-		// Мап для продуктов из заказа [id (primary key)]: product_variant_id
-		productsIDs := make(map[int]int)
-		rows, err := db.Query(
-			context.Background(),
-			`SELECT id, product_variant_id
-			FROM order_items
-			WHERE order_id = $1`,
-			commonOrderInfo.ID,
+			`SELECT id FROM orders WHERE status = 'paid'`,
 		)
 		if err != nil {
-			log.Printf("Ошибка получения айди продуктов в заказе из БД: %v", err)
+			log.Printf("Ошибка получения айди новых товаров из БД: %v", err)
 			c.JSON(http.StatusInternalServerError, gin.H{
 				"error": "Ошибка базы данных",
 			})
 			return
 		}
 
-		defer rows.Close()
+		defer ordersRows.Close()
 
-		for rows.Next() {
-			var id, productVariantID int
-			if err := rows.Scan(&id, &productVariantID); err != nil {
-				log.Printf("Ошибка чтения айди продуктов в заказе из БД: %v", err)
+		for ordersRows.Next() {
+			var id int
+			if err = ordersRows.Scan(&id); err != nil {
+				log.Printf("Ошибка чтения айди новых товаров из БД: %v", err)
 				c.JSON(http.StatusInternalServerError, gin.H{
 					"error": "Ошибка базы данных",
 				})
 				return
 			}
-			productsIDs[id] = productVariantID
+			newOrdersIDs = append(newOrdersIDs, id)
 		}
 
-		var order []OrderPiece
+		// Инициализацуем orders именно так, чтобы при отсутствии
+		// новых заказов на фронт пришел [], а не null
+		orders := []Order{}
+		for _, newOrderID := range newOrdersIDs {
 
-		for key, value := range productsIDs {
+			// Получение общей информации о заказе
+			var commonOrderInfo CommonOrderInfo
 
-			rows, err = db.Query(
+			err = db.QueryRow(
 				context.Background(),
-				`SELECT o.name, o."group", o.volume, o.unit
-				FROM options o
-				JOIN order_item_options oio ON oio.option_id = o.id
-				WHERE oio.order_item_id = $1`,
-				key,
-			)
+				`SELECT order_uuid, order_client_number, updated_at
+				FROM orders
+				WHERE id = $1`,
+				newOrderID,
+			).Scan(&commonOrderInfo.OrderUUID, &commonOrderInfo.OrderClientNumber, &commonOrderInfo.UpdatedAt)
 			if err != nil {
-				log.Printf("Ошибка получения информации об опциях в заказе из БД: %v", err)
+				log.Printf("Ошибка получения основной информации о товарах из БД: %v", err)
 				c.JSON(http.StatusInternalServerError, gin.H{
 					"error": "Ошибка базы данных",
 				})
 				return
 			}
-			
-			// Список опций для конкретного товара
-			var options []Option
-			for rows.Next() {
-				var option Option
-				if err := rows.Scan(&option.Name, &option.Group, &option.Volume, &option.Unit); err != nil {
-					log.Printf("Ошибка чтения информации об опциях в заказе из БД: %v", err)
+
+			// Мап для продуктов из заказа [id (primary key)]: product_variant_id
+			productsIDs := make(map[int]int)
+			itemRows, err := db.Query(
+				context.Background(),
+				`SELECT id, product_variant_id
+				FROM order_items
+				WHERE order_id = $1`,
+				newOrderID,
+			)
+			if err != nil {
+				log.Printf("Ошибка получения айди продуктов в заказе из БД: %v", err)
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"error": "Ошибка базы данных",
+				})
+				return
+			}
+
+			for itemRows.Next() {
+				var id, productVariantID int
+				if err := itemRows.Scan(&id, &productVariantID); err != nil {
+					log.Printf("Ошибка чтения айди продуктов в заказе из БД: %v", err)
 					c.JSON(http.StatusInternalServerError, gin.H{
 						"error": "Ошибка базы данных",
 					})
-					rows.Close()
+					itemRows.Close()
 					return
 				}
-				options = append(options, option)
+				productsIDs[id] = productVariantID
 			}
-			rows.Close()
+			itemRows.Close()
 
-			// Конкретный товар
-			var product Product
-            err = db.QueryRow(
-                context.Background(),
-                `SELECT name, volume, unit FROM product_variants WHERE id = $1`,
-                value,
-            ).Scan(&product.Name, &product.Volume, &product.Unit)
+			// Список товаров одного заказа
+			var goods []OrderPiece
+			for key, value := range productsIDs {
 
-            if err != nil {
-                log.Printf("Ошибка получения продукта: %v", err)
-                c.JSON(http.StatusInternalServerError, gin.H{"error": "Ошибка базы данных"})
-                return
-            }
+				optRows, err := db.Query(
+					context.Background(),
+					`SELECT o.name, o."group", o.volume, o.unit
+					FROM options o
+					JOIN order_item_options oio ON oio.option_id = o.id
+					WHERE oio.order_item_id = $1`,
+					key,
+				)
+				if err != nil {
+					log.Printf("Ошибка получения информации об опциях в заказе из БД: %v", err)
+					c.JSON(http.StatusInternalServerError, gin.H{
+						"error": "Ошибка базы данных",
+					})
+					return
+				}
+				
+				// Список опций для конкретного товара
+				var options []Option
+				for optRows.Next() {
+					var option Option
+					if err := optRows.Scan(&option.Name, &option.Group, &option.Volume, &option.Unit); err != nil {
+						log.Printf("Ошибка чтения информации об опциях в заказе из БД: %v", err)
+						c.JSON(http.StatusInternalServerError, gin.H{
+							"error": "Ошибка базы данных",
+						})
+						optRows.Close()
+						return
+					}
+					options = append(options, option)
+				}
+				optRows.Close()
 
-			piece := OrderPiece{
-				Product: product,
-				Options: options,
+				// Конкретный товар
+				var product Product
+				err = db.QueryRow(
+					context.Background(),
+					`SELECT name, volume, unit FROM product_variants WHERE id = $1`,
+					value,
+				).Scan(&product.Name, &product.Volume, &product.Unit)
+
+				if err != nil {
+					log.Printf("Ошибка получения продукта: %v", err)
+					c.JSON(http.StatusInternalServerError, gin.H{"error": "Ошибка базы данных"})
+					return
+				}
+
+				good := OrderPiece{
+					Product: product,
+					Options: options,
+				}
+				goods = append(goods, good)
+
 			}
 
-			order = append(order, piece)
+			order := Order{
+				CommonOrderInfo: commonOrderInfo,
+				Goods: goods,
+			}
+			
+			orders = append(orders, order)
 		}
 
+		c.JSON(http.StatusOK, gin.H{"orders": orders})
 	})
 }
